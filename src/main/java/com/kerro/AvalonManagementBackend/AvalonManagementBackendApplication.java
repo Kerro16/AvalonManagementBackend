168e5c29584cd2497617cd571e223266b4708ddf
kerro16 <kerrox16@gmail.com>
2025-07-03 00:52:27 -0600

    git init

---
+++ src/main/java/com/kerro/AvalonManagementBackend/AvalonManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kerro.AvalonManagementBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvalonManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvalonManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kerro/AvalonManagementBackend/AvalonManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerro.AvalonManagementBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvalonManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
